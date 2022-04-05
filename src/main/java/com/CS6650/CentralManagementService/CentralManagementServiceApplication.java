2cf5129044e86322d2ab34c252d0d3c0e8291dad
pdharme <dharme.p@husky.neu.edu>
2022-04-05 15:39:13 -0400

    Initializing Spring Boot project

---
+++ src/main/java/com/CS6650/CentralManagementService/CentralManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.CS6650.CentralManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CS6650/CentralManagementService/CentralManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CS6650.CentralManagementService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
